fca724b080cd66818c842a7e87df7a7331ab4728
Charles Kim <charles.kim@smoothstack.com>
2021-04-15 02:16:52 -0400

    Add search button

--- src/app/airplane-search/airplane-search.component.ts
+++ src/app/airplane-search/airplane-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 import {
     debounceTime, distinctUntilChanged, switchMap
@@ -13,16 +14,36 @@ import { AirplaneService } from '../airplane.service';
 })
 export class AirplaneSearchComponent implements OnInit {
     airplanes$!: Observable<Airplane[]>;
+    selectedAirplaneId: number = 0;
+    selectedAirplane!: Airplane;
+
     private searchTerms = new Subject<string>();
 
-    constructor(private airplaneService: AirplaneService) { }
+    constructor(private airplaneService: AirplaneService, private router: Router) { }
 
     // Push a search term into the observable stream.
     search(term: string): void {
         this.searchTerms.next(term);
     }
 
+    goToDetails(): void {
+        if (this.selectedAirplaneId >= 1) {
+            this.router.navigateByUrl("/detail/" + this.selectedAirplaneId);
+        }
+    }
+
+    updateSearchBox(searchBox: HTMLInputElement, listItem: HTMLLIElement, airplane: Airplane): void {
+        searchBox.value = airplane.model;
+        this.selectedAirplaneId = airplane.id
+        this.initializeAirplanes();
+    }
+
     ngOnInit(): void {
+        this.initializeAirplanes();
+        this.selectedAirplane = {} as Airplane;
+    }
+
+    initializeAirplanes(): void {
         this.airplanes$ = this.searchTerms.pipe(
             // wait 300ms after each keystroke before considering the term
             debounceTime(300),
