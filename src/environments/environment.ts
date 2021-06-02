06c8d3d2972dc16a4bbde5084d18039c4773250d
Charles Kim <charles.kim@smoothstack.com>
2021-06-01 23:00:25 -0400

    Update Jenkinsfile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'http://utopia-load-balancer-1166047770.us-east-2.elb.amazonaws.com'
 };
 
 /*
