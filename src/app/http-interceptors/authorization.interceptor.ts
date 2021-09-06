02643e41de0572f9b1c3eac9d358e08823295dea
anthsirimar <anthony.sirimarco@smoothstack.com>
2021-09-06 13:14:39 -0700

    User is now logged out when jwt expires

--- src/app/http-interceptors/authorization.interceptor.ts
+++ src/app/http-interceptors/authorization.interceptor.ts
@@ -1,16 +1,21 @@
 import {
+    HttpErrorResponse,
     HttpEvent,
     HttpHandler,
     HttpInterceptor,
-    HttpRequest
+    HttpRequest,
+    HttpResponse
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LoginService } from '../services/login.service';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
-    constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private router: Router, private location: Location) { }
 
     intercept(
         request: HttpRequest<unknown>,
@@ -19,6 +24,17 @@ export class AuthorizationInterceptor implements HttpInterceptor {
         request = request.clone({
             setHeaders: { Authorization: this.loginService.getToken() }
         });
-        return next.handle(request);
-    }
+      return next.handle(request)
+        //handles logging the user out if the token has expired
+        .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 403) {
+            this.loginService.logout();
+            this.loginService.setPreviousPage(this.location.path());
+            this.router.navigate(['/login']);
+          }
+          return next.handle(request);
+        })
+      )
+  }
 }
