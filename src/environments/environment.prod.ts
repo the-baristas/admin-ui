58391aa8d8095a57c3f07be0673af80fb6928e51
anthsirimar <anthony.sirimarco@smoothstack.com>
2021-11-09 11:12:18 -0800

    edit environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com',
+    apiUrl: 'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com:8080',
     userServiceUrl:
-        'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com',
+        'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com:8081',
     flightServiceUrl:
-        'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com',
+        'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com:8090',
     bookingServiceUrl:
-        'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com'
+        'http://utopia-load-balancer-374412060.us-east-2.elb.amazonaws.com:8091'
 };
